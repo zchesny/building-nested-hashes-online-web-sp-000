70376e7a436395895033e90805a655b9c85833d9
Zoey Chesny <z.chesny@gmail.com>
2020-04-13 17:59:14 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,11 +18,11 @@ def third_challenge
         status: "alive"
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
-        name: "Lord Capulet", 
+        name: "Lord Capulet",
         age: "50"
         },
       :matriarch => {
@@ -40,5 +40,5 @@ def third_challenge
    }
   }
 
-  
+
 end
